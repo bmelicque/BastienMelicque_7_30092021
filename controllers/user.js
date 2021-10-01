9e6252b00077f9fe6a4671d742743441d2432da7
Bastien Mélicque <bmelicque@gmail.com>
2021-10-01 10:59:24 +0200

    Some validations before signup

--- controllers/user.js
+++ controllers/user.js
@@ -1,3 +1,4 @@
+const { isEmail } = require('validator');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const cryptoJS = require('crypto-js');
@@ -5,18 +6,29 @@ const db = require('../config/db');
 
 
 // On signup, the email is hashed to respect GDPR (no personal data stored on the server)
-// Emails have a unicity constraint within the database
 // The password is encrypted for security reasons
 exports.signup = async (req, res, next) => {
     try {
+        // Checking if the email is valid
+        if (!isEmail(req.body.email)) return res.status(400).json({ error: 'Adresse email incorrecte' });
+
         const hashedEmail = cryptoJS.SHA3(req.body.email).toString();
+
+        // Checking email unicity
+        const emailSQL = `SELECT * FROM users WHERE users.email = '${hashedEmail}'`;
+        const emailIsUnique = await (await db).query(emailSQL);
+        if (emailIsUnique) return res.status(400).json({ error: 'Cet email est déjà utilisé' });
+
+        // Checking username unicity
+        const userSQL = `SELECT * FROM users WHERE users.username = '${req.body.username}'`;
+        const user = await (await db).query(userSQL);
+        if (user) return res.status(400).json({ error: 'Ce pseudo est déjà utilisé' });
+
+        // Actual post to database
         const hashedPassword = await bcrypt.hash(req.body.password, 10);
-        
         const sql = `INSERT INTO users (username, email, password) VALUES ('${req.body.username}', '${hashedEmail}', '${hashedPassword}')`;
-
-        (await db).query(sql)
-            .then(res.status(201).json({ message: 'Utilisateur créé avec succès' }))
-
+        await (await db).query(sql)
+        res.status(201).json({ message: 'Utilisateur créé avec succès' })
     } catch (err) {
         res.status(500).json({ err })
     }
@@ -28,15 +40,17 @@ exports.signup = async (req, res, next) => {
 // On success, provides a secured token to the user
 exports.login = async (req, res, next) => {
     try {
+        // Checking if email exists in the database
         const hashedEmail = cryptoJS.SHA3(req.body.email).toString();
-        const findUserSQL = `SELECT * FROM users WHERE users.email = '${hashedEmail}'`;
-
-        const user = await (await db).query(findUserSQL);
+        const sql = `SELECT * FROM users WHERE users.email = '${hashedEmail}'`;
+        const user = await (await db).query(sql);
         if (!user) return res.status(401).json({ error: 'Utilisateur inexistant' })
 
+        // Checking if the password is valid
         const valid = await bcrypt.compare(req.body.password, user[0].password)
         if (!valid) return res.status(401).json({ error: 'Mot de passe incorrect' })
 
+        // Sending a token to the user
         res.status(200).json({
             userId: user[0].id,
             token: jwt.sign(
